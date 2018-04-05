5084968f4cc7e269eec5409e54b1e8d25ea271ac
Alexandre Annic <alexandre.annic1@gmail.com>
2018-04-05 18:32:47 +0200

    Fix reducer opiton issue on firefox

--- src/lib/conf/reducer.js
+++ src/lib/conf/reducer.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunk from "redux-thunk";
 import {formReducer} from "../Form";
 
@@ -6,10 +6,7 @@ export const store = createStore(
     combineReducers({
         formAnswer: formReducer
     }),
-    compose(process.env.NODE_ENV === 'production'
-            ? applyMiddleware(thunk)
-            : applyMiddleware(thunk),
-        // Redux DevToops Chrome plugins
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    applyMiddleware(thunk)
+    // Redux DevToops Chrome plugins
+    // ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
