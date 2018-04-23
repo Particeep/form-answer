81992205714c739eb06310db1914d041aea72e73
Alexandre Annic <alexandre.annic1@gmail.com>
2018-04-23 18:26:56 +0200

    Clean webpack config

--- conf/webpack.compile.js
+++ conf/webpack.compile.js
@@ -1,35 +1,15 @@
+const webpackMerge = require('webpack-merge');
+const commonConfig = require('./webpack.js');
 const path = require('path');
 
-module.exports = {
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
-    },
-    module: {
-        rules: [
-            {test: /\.tsx?$/, loader: "ts-loader"},
-            {
-                test: /\.jsx?$/,
-                include: path.resolve('./src'),
-                exclude: /(node_modules|bower_components|build)/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            }, {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader?importLoaders=1'
-            }, {
-                test: /\.scss$/,
-                loader: 'style-loader!css-loader?importLoaders=1&localIdentName=[local]_[hash:base64:5]!sass-loader'
-            }
-        ]
-    },
+module.exports = webpackMerge(commonConfig, {
     externals: {
         'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
     },
-    entry: path.resolve('./src/lib/index.js'),
+    entry: path.resolve('./src/lib/index.ts'),
     output: {
-        path: path.resolve('./build'),
+        path: path.resolve('./compile'),
         filename: 'index.js',
         libraryTarget: 'commonjs2'
     },
-};
+});
--- conf/webpack.start.js
+++ conf/webpack.start.js
@@ -1,23 +1,17 @@
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.js');
+const webpackMerge = require('webpack-merge');
 const path = require('path');
 
-module.exports = {
+module.exports = webpackMerge(commonConfig, {
     cache: true,
     devtool: 'eval-source-map',
     target: 'web',
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
-    },
     entry: {
-        'app': path.resolve('src/app/index.tsx'),
+        'app': ['babel-polyfill', path.resolve('src/app/index.tsx')],
         'utils': require.resolve('react-dev-utils/webpackHotDevClient'),
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: path.resolve('src/app/index.html'),
-        }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
@@ -26,25 +20,6 @@ module.exports = {
             }
         })
     ],
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: "ts-loader" },
-            {
-                test: /\.jsx?$/,
-                include: path.resolve('./src'),
-                exclude: /(node_modules|bower_components|build)/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            }, {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader?importLoaders=1'
-            }, {
-                test: /\.scss$/,
-                loader: 'style-loader!css-loader?importLoaders=1&localIdentName=[local]_[hash:base64:5]!sass-loader'
-            }
-        ]
-    },
     output: {
         path: path.resolve('./dist'),
         publicPath: 'http://localhost:3333/',
@@ -62,4 +37,4 @@ module.exports = {
             poll: 300
         }
     },
-};
+});
