090555dd5c89db8b14c2be9dde41a75adc44920b
Fhammache <faredj.hammache@particeep.com>
2019-02-12 11:24:04 +0100

    fix urlify fct

--- src/lib/utils/common.ts
+++ src/lib/utils/common.ts
@@ -21,6 +21,9 @@ export const stringToDate = (date: string, format: string): Date => {
 };
 
 export function urlify(text) {
+  const getHostName = (url: string): string => new URL(url).hostname;
   let urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-  return text.replace(urlRegex, url => `<a href='${url}' class="Form_a" target='_blank'>${url}</a>`);
+  if (text != undefined) {
+    return text.replace(urlRegex, url => `<a href='${url}' class="Form_a" target='_blank'>${getHostName(url)}</a>`);
+  }
 };
