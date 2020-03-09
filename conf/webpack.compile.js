8347b50167b8d4ac6a43778ccbc3f1ebd9b65785
fhammache <faredj.hammache@particeep.com>
2020-03-09 17:52:27 +0100

    webpack compile

--- conf/webpack.compile.js
+++ conf/webpack.compile.js
@@ -3,9 +3,6 @@ const commonConfig = require('./webpack.js');
 const path = require('path');
 
 module.exports = webpackMerge(commonConfig, {
-    externals: {
-        'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-    },
     entry: path.resolve('./src/lib/index.ts'),
     output: {
         path: path.resolve('./compile'),
