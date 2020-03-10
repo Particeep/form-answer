8dc958fd3f3a11327901a1154c2cab7b48466638
fhammache <faredj.hammache@particeep.com>
2020-03-10 10:14:37 +0100

    webpack compile

--- conf/webpack.compile.js
+++ conf/webpack.compile.js
@@ -3,6 +3,17 @@ const commonConfig = require('./webpack.js');
 const path = require('path');
 
 module.exports = webpackMerge(commonConfig, {
+    externals: [
+      {
+        react: {
+          root: 'React',
+          commonjs2: 'react',
+          commonjs: ['react'],
+          amd: 'react',
+        },
+      },
+      /@material-ui\/core\/.*/,
+    ],
     entry: path.resolve('./src/lib/index.ts'),
     output: {
         path: path.resolve('./compile'),
